89ece68d4ff7cac2efcff948ff081a116e6b0ed9
Abra19 <evabrarova@gmail.com>
2024-01-26 21:10:03 +0300

    customers analytics add

--- queries.sql
+++ queries.sql
@@ -41,6 +41,10 @@ SELECT
 	  FLOOR(s.average_income) as average_income
 FROM employees AS e
 INNER JOIN sales_avg AS s
+  ON e.employee_id = s.sales_person_id
+CROSS JOIN overall_avg AS ova
+WHERE average_income < ova.all_avg
+ORDER BY average_income;
 
 -- sales by salesmans an weekdays
 
@@ -65,3 +69,55 @@ SELECT
 FROM weekday_sales
 GROUP BY name, weekday, day_number
 ORDER BY name, day_number;
+
+-- customers categories
+
+SELECT
+	  CASE
+		    WHEN age > 40 THEN '40+'
+		    WHEN age <= 40 AND age >= 26 THEN '26-40'
+		    WHEN age >= 16 AND age < 26 THEN '16-25'
+		    ELSE 'small'
+	  END AS age_category,
+		count(customer_id) AS count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category;
+
+-- customers by months
+
+SELECT
+	  CONCAT(EXTRACT(YEAR FROM s.sale_date), '-', TO_CHAR(sale_date, 'MM')) AS date,
+	  COUNT(s.customer_id) AS total_customers,
+	  FLOOR(SUM(s.quantity * p.price)) AS income
+FROM sales s
+INNER JOIN products p
+	  ON s.product_id = p.product_id
+GROUP BY date
+ORDER BY date;
+
+-- sales in special offers
+
+WITH first_sale AS (
+	  SELECT DISTINCT
+		    c.customer_id AS customer_id,
+		    s.sales_person_id AS sales_person_id, 
+		    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+		    FIRST_VALUE(s.sale_date) OVER (PARTITION BY s.customer_id) AS first_sale,
+		    p.price AS price
+	  FROM sales AS s
+	  INNER JOIN products AS p
+		    ON s.product_id = p.product_id 
+	  INNER JOIN customers AS c 
+		    ON s.customer_id  = c.customer_id
+)
+
+SELECT
+	  f.customer AS customer,
+	  f.first_sale AS sale_date,
+	  CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM first_sale AS f
+INNER JOIN employees AS e
+	  ON f.sales_person_id = e.employee_id
+WHERE f.price = 0
+ORDER BY customer_id;
